51a91e2eb8569c9f272fd60f1f2fd0870625b715
h96ven <33.heavens@gmail.com>
2021-10-02 20:26:54 +0300

    Final project version of dynasties, 2 october 2021

---
+++ dynasties/dynasties.py
@@ -0,0 +1,12 @@
+dynasties_list = [['The Merovingians', 'V-VIII centuries', \
+'Clovis I', '../static/Clovis_1er.jpg'], ['The Carolingians', \
+'751–987', 'Pepin III the Short', '../static/Amiel_-_Pepin_the_Short.jpg'], \
+['The Capetians', '987–1328', 'Hugh Capet', '../static/King_Hugh_Capet.jpg'], \
+['The Valois', '1328–1589', 'Philip VI the Fortunate', \
+'../static/Robert-Fleury_-_Philip_VI_of_France.jpg'], \
+['The Bourbons', '1589–1792, 1815–1830', 'Henry IV the Green Gallant', \
+'../static/King_Henry_IV_of_France.jpg'], ['The Bonapartes', '1804–1814, \
+1852–1870', 'Napoleon Bonaparte', \
+'../static/Napoleon_I_of_France_by_Andrea_Appiani.jpg'], \
+['The Orleans', '1830–1848', 'Louis Phillippe', \
+'../static/Louis_philippe_1835_par_desnos.jpeg']]
